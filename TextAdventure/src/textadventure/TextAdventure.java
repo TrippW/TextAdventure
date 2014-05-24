2292bd445a52cee604f806b02806904483319a51
TrippW <trwlll@mindspring.com>
2014-05-24 15:04:05 -0500

    Maven Files
    The start to Maven
    
    Use The HauntedHorror Files

---
+++ TextAdventure/src/textadventure/TextAdventure.java
@@ -0,0 +1,14 @@
+package textadventure;
+
+/**
+ *
+ * @author Tripp
+ */
+public class TextAdventure {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
