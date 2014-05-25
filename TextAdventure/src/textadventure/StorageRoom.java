eadfe7f4cc7a33ff26216c451ede5fcb3eea548d
TrippW <trwlll@mindspring.com>
2014-05-25 08:28:59 -0500

    Fixed all

--- TextAdventure/src/textadventure/StorageRoom.java
+++ TextAdventure/src/textadventure/StorageRoom.java
@@ -1,28 +1,24 @@
 package textadventure;
 
-import concepts.Rooms;
+public class StorageRoom extends Rooms {
 
-public class StorageRoom extends Rooms{
-	
-	public String getDescription()
-	{
-		return "Carcinogenic solvents, mops, brooms, and, inexplicably, an axe. There is a faint squeaking. Obviously, no one came in here much.";
-	}
+    public StorageRoom() {
+        super("Storage Room");
+    }
 
-	public void getItems()
-	{
-    items[0] = Cabinet1 ;
-    items[1] = Cabinet2;
-	}
-	public void takeItem(Iteams itemChoice)
-	{
-		for(int i=0; i>=items.length(); i++)
-		{
-			if(items[i] == iteamChoice)
-			{
-				items[i] = null;
-				// Put item into inv
-			}
-		}
-	}
+    public String getDescription() {
+        return "Carcinogenic solvents, mops, brooms, and, inexplicably, an axe. There is a faint squeaking. Obviously, no one came in here much.";
+    }
+
+    public void getItems() {
+    }
+
+    public void takeItem(Items itemChoice) {
+//        for (int i = 0; i >= items.length(); i++) {
+//            if (items[i] == iteamChoice) {
+//                items[i] = null;
+//                // Put item into inv
+//            }
+//        }
+    }
 }
